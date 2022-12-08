25aaa812f9601039aded5ba7642768455d220821
saharadam45 <saharadam45@gmail.com>
2022-12-08 12:51:57 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
- * main - Entry
- * Return:Always 0
+ * main-entry point for the program.
+ *
+ * Return:0 if no errors, return non zero if errors
  */
 int main(void)
 {
-	printf("with proper") 
-			return(0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
+
