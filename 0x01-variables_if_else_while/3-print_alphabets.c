255670f91927e38279c3870a6401a66d9935e339
saharadam45 <saharadam45@gmail.com>
2022-12-12 16:57:40 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints alpha in lower case
+ * Return: always 0
+ */
+int main(void)
+{
+	char s;
+
+	for (s = 'a'; s <= 'z'; s++)
+	{
+		putchar(s);
+	}
+	for (s = 'A'; s <= 'Z'; s++)
+		putchar(s);
+	}
+	putchar('\n');
+	return (0);
+}
