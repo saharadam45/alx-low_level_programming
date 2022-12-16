3b6bdd7bd43f7d10e4c0cded526c1011ce5644f1
saharadam45 <saharadam45@gmail.com>
2022-12-16 12:37:26 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 
-/**
- * print_alphabet - print all alphabet in lowercase
-*/
+	/**
+	 * print_alphabet - print all alphabet in lowercase
+	 */
 
 
 	void print_alphabet(void)
@@ -13,4 +13,7 @@
 
 		for (letter = 'a'; letter <= 'z'; letter++)
 			_putchar(letter);
-	{
+
+
+		_putchar('\n');
+	}
